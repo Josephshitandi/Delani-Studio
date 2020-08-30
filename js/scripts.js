4fa03e1cf45e2f5a3b7443d5570b180b59264720
Josephshitandi <jsphshtnd@gmail.com>
2020-08-30 07:53:46 +0300

    Add jquery function for development

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
-$(document).ready(function(){
-    $("#design-pic").click(function(){
+$(document).ready(function () {
+    $("#design-pic").click(function () {
         $("#design-p").show();
-        $("design-pic").hide();
-    });
-    $("#design-p").click(function(){
+        $("#design-pic").hide();
+    })
+    $("#design-p").click(function () {
         $("#design-p").hide();
         $("#design-pic").show();
-    });    
-})
+    })
+    $("#development-pic").click(function () {
+        $("#development-p").show();
+        $("#development-pic").hide();
+    })
+    $("#development-p").click(function () {
+        $("#development-p").hide();
+        $("#development-pic").show();
+    });
+});
